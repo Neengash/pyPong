ca014b584b11dbd02ccd0ef64c5cb403840531dd
Pep Mañas <pepmanas93@gmail.com>
2017-07-15 15:38:38 +0200

    movement bars inside the pong world

--- actors/bar.py
+++ actors/bar.py
@@ -10,9 +10,24 @@ class Bar(Actor):
 
     def __init__(self, state, player):
         screen_rect = state.get_screen().get_rect()
-        self.posx = screen_rect.left + self.width / 2
+        self.posx = self.get_player_posx(screen_rect, player)
         self.posy = screen_rect.top / 2
         self.reset_bar_state()
+        self.set_movement_keys(player)
+
+    def get_player_posx(self, screen_rect, player):
+        if player == 1:
+            return screen_rect.left + self.width / 2
+        elif player == 2:
+            return screen_rect.right - self.width * 3 / 2
+
+    def set_movement_keys(self, player):
+        if player == 1:
+            self.key_up = pygame.K_q
+            self.key_down = pygame.K_a
+        elif player == 2:
+            self.key_up = pygame.K_p
+            self.key_down = pygame.K_l
 
     def reset_bar_state(self):
         self.bar_state = {
@@ -22,10 +37,22 @@ class Bar(Actor):
 
     def check_inputs(self, state):
         self.reset_bar_state()
-        if state.get_keys()[pygame.K_q]:
+        if self.check_go_up(state):
             self.bar_state['up'] = True
-        if state.get_keys()[pygame.K_a]:
+        if self.check_go_down(state):
             self.bar_state['down'] = True
+
+    def check_go_up(self, state):
+        return (
+            state.get_keys()[self.key_up] and
+            self.posy > 0
+        )
+
+    def check_go_down(self, state):
+        return (
+            state.get_keys()[self.key_down] and
+            self.posy < state.get_screen().get_rect().height - self.height
+        )
 
     def update(self, state):
         if self.bar_state['up']:
