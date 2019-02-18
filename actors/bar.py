948a1cbceb57e86a1dade6238cbdc44e48259be7
Neengash <pepmanas93@gmail.com>
2019-02-18 22:37:28 +0100

    clean bar class and add bar_drawer

--- actors/bar.py
+++ actors/bar.py
@@ -1,67 +1,31 @@
 import pygame
 from actors.actor import Actor
+from drawers.bar_drawer import BarDrawer
 
 class Bar(Actor):
 
     speed = 5
     width = 10
     height = 50
-    bar_state = {}
+    drawer = None
+    body = None
 
-    def __init__(self, state, player):
-        self.state = state
-        screen_rect = self.state.get_screen().get_rect()
-        self.posx = self.get_player_posx(screen_rect, player)
-        self.posy = (screen_rect.bottom / 2) - self.width / 2
-        self.reset_bar_state()
-        self.set_movement_keys(player)
-
-    def get_player_posx(self, screen_rect, player):
-        base_separation = self.width / 2
-        if player == 1:
-            return screen_rect.left + base_separation
-        elif player == 2:
-            return screen_rect.right - self.width - base_separation
-
-    def set_movement_keys(self, player):
-        if player == 1:
-            self.key_up = pygame.K_q
-            self.key_down = pygame.K_a
-        elif player == 2:
-            self.key_up = pygame.K_p
-            self.key_down = pygame.K_l
-
-    def reset_bar_state(self):
-        self.bar_state = {
-            'up'   : False,
-            'down' : False,
-        }
-
-    def check_inputs(self, keys):
-        self.reset_bar_state()
-        if self.check_go_up(keys):
-            self.bar_state['up'] = True
-        if self.check_go_down(keys):
-            self.bar_state['down'] = True
-
-    def check_go_up(self, keys):
-        return (
-            keys[self.key_up] and self.posy > 0
-        )
-
-    def check_go_down(self, keys):
-        return (
-            keys[self.key_down] and
-            self.posy < self.state.get_screen().get_rect().height - self.height
-        )
+    def __init__(self, posx, posy, bar_drawer, bar_body):
+        self.posx = posx
+        self.posy = posy
+        self.vely = 0
+        self.accy = 0
+        self.drawer = bar_drawer
+        self.body = bar_body
 
     def update(self):
-        if self.bar_state['up']:
-            self.posy -= Bar.speed
-        if self.bar_state['down']:
-            self.posy += Bar.speed
+        self.vely += self.accy
+        self.posy += self.vely
 
     def draw(self):
-        screen = self.state.get_screen()
-        pygame.draw.rect(screen, (255, 255, 255),
-            [self.posx, self.posy, self.width, self.height])
+        self.drawer.draw(
+            self.posx, 
+            self.posy,
+            self.width,
+            self.height
+        )
